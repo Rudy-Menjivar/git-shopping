eb77e23d75a1326783739665f7751b7274f4aebe
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-22 19:07:57 -0800

    Corrected all lint errors but disabling some that aren't necessary

--- models/db.js
+++ models/db.js
@@ -1,118 +1,139 @@
-const Sequelize = require('sequelize')
-const bcrypt = require('bcryptjs')
+/* eslint-disable no-var */
+/* eslint-disable prettier/prettier */
+/* eslint-disable camelcase */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+/* eslint-disable vars-on-top */
 
-var env = process.env.NODE_ENV || 'development';
-var config = require('../config/config.json')[env];
+const Sequelize = require("sequelize");
+const bcrypt = require("bcryptjs");
+
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config.json")[env];
 
 if (config.use_env_variable) {
-	var sequelize = new Sequelize(process.env[config.use_env_variable]);
-  } else {
-	var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 const globalModelConfig = {
-	underscored: true,
-	timestamps: true,
-}
+  underscored: true,
+  timestamps: true
+};
 
-sequelize.authenticate()
-	.then(() => {
-		// eslint-disable-next-line no-console
-		console.log('Connection has been established successfully.')
-	})
-	.catch((err) => {
-		// eslint-disable-next-line no-console
-		console.error('Unable to connect to the database:', err)
-	})
+sequelize
+  .authenticate()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    // eslint-disable-next-line no-console
+    console.error("Unable to connect to the database:", err);
+  });
 
-const SessionModel = sequelize.define('Session', {
-	sid: {
-		type: Sequelize.STRING,
-		primaryKey: true
-	},
-	expires: Sequelize.DATE,
-	data: Sequelize.STRING(10000),
-}, globalModelConfig)
+const SessionModel = sequelize.define(
+  "Session",
+  {
+    sid: {
+      type: Sequelize.STRING,
+      primaryKey: true
+    },
+    expires: Sequelize.DATE,
+    data: Sequelize.STRING(10000)
+  },
+  globalModelConfig
+);
 
-const UserModel = sequelize.define('User', {
-	uid: {
-		type: Sequelize.INTEGER,
-		primaryKey: true,
-		autoIncrement: true,
-	},
-	username: Sequelize.STRING(30),
-	email: Sequelize.STRING(255),
-	password_hash: Sequelize.STRING(255),
-}, globalModelConfig)
+const UserModel = sequelize.define(
+  "User",
+  {
+    uid: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: Sequelize.STRING(30),
+    email: Sequelize.STRING(255),
+    password_hash: Sequelize.STRING(255)
+  },
+  globalModelConfig
+);
 
 sequelize.sync({
-	alter: true
-})
+  alter: true
+});
 
-const getUserById = uid => UserModel.findOne({ where: { uid } })
-const getUserByUsername = username => UserModel.findOne({ where: { username } })
-const getUserByEmail = email => UserModel.findOne({ where: { email } })
+const getUserById = uid => UserModel.findOne({ where: { uid } });
+const getUserByUsername = username => UserModel.findOne({ where: { username } });
+const getUserByEmail = email => UserModel.findOne({ where: { email } });
 
 const isUsernameInUse = async username => {
-	return await getUserByUsername(username) !== null
-}
+  return (await getUserByUsername(username)) !== null;
+};
 
 const isEmailInUse = async email => {
-	return (await getUserByEmail(email) ? true : false)
-}
+  return (await getUserByEmail(email)) ? true : false;
+};
 
 const createUserRecord = userObj => new Promise(async (resolve, reject) => {
-	const passwdHash = await createPasswordHash(userObj.password)
-	UserModel.create({
-		email: userObj.email,
-		username: userObj.username,
-		password_hash: passwdHash
-	})
-		.then((createdUser) => {
-			resolve(createdUser)
-		})
-		.catch(err => reject(err))
-})
+  const passwdHash = await createPasswordHash(userObj.password);
+  UserModel.create({
+    email: userObj.email,
+    username: userObj.username,
+    password_hash: passwdHash
+  })
+    .then((createdUser) => {
+      resolve(createdUser);
+    })
+    .catch(err => reject(err));
+});
 
 const createPasswordHash = password => new Promise(async (resolve, reject) => {
-	try {
-		const saltRounds = 10
-		bcrypt.hash(password, saltRounds, (err, hash) => {
-			resolve(hash)
-		})
-	}
-	catch (err) {
-		reject(err)
-	}
-})
+  try {
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      resolve(hash);
+    });
+  }
+  catch (err) {
+    reject(err);
+  }
+});
 
 const isPasswordHashVerified = (hash, password) => new Promise(async (resolve, reject) => {
-	try {
-		bcrypt.compare(password, hash, (err, res) => {
-			resolve(res)
-		})
-	}
-	catch (err) {
-		reject(err)
-	}
-})
+  try {
+    bcrypt.compare(password, hash, (err, res) => {
+      resolve(res);
+    });
+  }
+  catch (err) {
+    reject(err);
+  }
+});
 
-module.exports = (session) => {
-	const SequelizeStore = require('connect-session-sequelize')(session.Store)
-	
-	const SessionStore = new SequelizeStore({
-		db: sequelize,
-		table: 'Session'
-	})
+module.exports = session => {
+  const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-	return {
-		SessionStore,
-		getUserById,
-		getUserByUsername,
-		getUserByEmail,
-		isUsernameInUse,
-		isEmailInUse,
-		createUserRecord,
-		isPasswordHashVerified,
-	}
-}
+  const SessionStore = new SequelizeStore({
+    db: sequelize,
+    table: "Session"
+  });
+
+  return {
+    SessionStore,
+    getUserById,
+    getUserByUsername,
+    getUserByEmail,
+    isUsernameInUse,
+    isEmailInUse,
+    createUserRecord,
+    isPasswordHashVerified
+  };
+};
